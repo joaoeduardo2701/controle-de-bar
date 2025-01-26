42f2cc19cb093afb9b2831837a06e22e588cc610
Dudu <duducardoso2701@gmail.com>
2025-01-25 21:17:30 -0300

    Módulo Mesa finalizada

---
+++ ControleDeBar.Dominio/ModuloMesa/Mesa.cs
@@ -0,0 +1,43 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Dominio.ModuloMesa;
+public class Mesa : EntidadeBase
+{
+    public string Numero { get; set; }
+    public bool Ocupada { get; set; }
+
+    public Mesa() {}
+
+    public Mesa(string numeroMesa) : this()
+    {
+        Numero = numeroMesa;
+    }
+
+    public void Ocupar()
+    {
+        Ocupada = true;
+    }
+
+    public void Desocupar()
+    {
+        Ocupada = false;
+    }
+
+    public override void AtualizarInformacoes(EntidadeBase registroAtualizado)
+    {
+        Mesa mesaAtualizada = (Mesa)registroAtualizado;
+
+        Numero = mesaAtualizada.Numero;
+        Ocupada = mesaAtualizada.Ocupada;
+    }
+
+    public override List<string> Validar()
+    {
+        List<string> erros = new List<string>();
+
+        if (string.IsNullOrEmpty(Numero.Trim()))
+            erros.Add("O campo \"Número da Mesa\" é obrigatorio");
+
+        return erros;
+    }
+}

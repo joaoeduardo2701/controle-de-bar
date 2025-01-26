d6a2563573e6220674eff05691f8db0e1a2ab96e
Dudu <duducardoso2701@gmail.com>
2025-01-25 21:00:35 -0300

    Classe Entidade Base criada

---
+++ ControleDeBar.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleDeBar.Dominio.Compartilhado;
+public abstract class EntidadeBase
+{
+    public int Id { get; set; }
+
+    public abstract void AtualizarInformacoes(EntidadeBase registroAtualizado);
+
+    public abstract List<string> Validar();
+}

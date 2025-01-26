e4c570603bc1412c0aab8b85a781a4bf20f22cc3
Dudu <duducardoso2701@gmail.com>
2025-01-25 21:02:33 -0300

    IRepositorioBase criado

---
+++ ControleDeBar.Dominio/Compartilhado/IRepositorioBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleDeBar.Dominio.Compartilhado;
+public interface IRepositorioBase<TEntidade> where TEntidade : EntidadeBase
+{
+    void Inserir(TEntidade registro);
+    bool Editar(TEntidade registroOriginal, TEntidade registroAtualizado);
+    bool Excluir(TEntidade registro);
+    TEntidade SelecionarPorId(int id);
+    List<TEntidade> SelecionarTodos();
+}

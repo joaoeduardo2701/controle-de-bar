8d208fc7efcaf2c14644be3154f07690f3f600b6
Dudu <duducardoso2701@gmail.com>
2025-01-27 18:25:14 -0300

    Repositório de Produto implementado

---
+++ ControleDeBar.Infraestrutura/ModuloProduto/RepositorioProdutoEmOrm.cs
@@ -0,0 +1,57 @@
+﻿using ControleDeBar.Dominio.ModuloProduto;
+using ControleDeBar.Infraestrutura.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.ModuloProduto;
+public class RepositorioProdutoEmOrm : IRepositorioProduto
+{
+    ControleDeBarDbContext dbContext { get; set; }
+
+    public RepositorioProdutoEmOrm(ControleDeBarDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public void Inserir(Produto registro)
+    {
+        dbContext.Produtos.Add(registro);
+
+        dbContext.SaveChanges();
+    }
+
+    public bool Editar(Produto registroOriginal, Produto registroAtualizado)
+    {
+        if (registroOriginal == null || registroAtualizado == null)
+            return false;
+
+        registroAtualizado.AtualizarInformacoes(registroAtualizado);
+
+        dbContext.Produtos.Update(registroOriginal);
+
+        dbContext.SaveChanges();
+
+        return true;
+
+    }
+
+    public bool Excluir(Produto registro)
+    {
+        if (registro == null)
+            return false;
+
+        dbContext.Produtos.Remove(registro);
+
+        dbContext.SaveChanges();
+
+        return true;
+    }
+
+    public Produto SelecionarPorId(int id)
+    {
+        return dbContext.Produtos.Find(id)!;
+    }
+
+    public List<Produto> SelecionarTodos()
+    {
+        return dbContext.Produtos.ToList();
+    }
+}

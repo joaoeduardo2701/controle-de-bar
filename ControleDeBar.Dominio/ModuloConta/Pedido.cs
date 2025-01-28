edd29eb6bfe8ba4198b7c1dd7040b7779d68f2e0
Dudu <duducardoso2701@gmail.com>
2025-01-28 12:54:08 -0300

    Módulo Auxiliar Pedido criado

---
+++ ControleDeBar.Dominio/ModuloConta/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeBar.Dominio.ModuloProduto;
+
+namespace ControleDeBar.Dominio.ModuloConta;
+public class Pedido
+{
+    public int id { get; set; }
+    public Produto Produto { get; set; }
+    public int QuantidadeSolicitada { get; set; }
+
+    public Pedido() { }
+
+    public Pedido(int id, Produto produto, int quantidadeSolicitada)
+    {
+        this.id = id;
+        Produto = produto;
+        QuantidadeSolicitada = quantidadeSolicitada;
+    }
+
+    public decimal CalculaTotalParcial()
+    {
+        return Produto.Valor * QuantidadeSolicitada;
+    }
+}

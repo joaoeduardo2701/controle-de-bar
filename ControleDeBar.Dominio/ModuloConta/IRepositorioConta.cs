a8ad633f3c84d7c652abfaa5d872566199e53ed6
Dudu <duducardoso2701@gmail.com>
2025-01-28 14:32:49 -0300

    IRepositorioConta adicionado

---
+++ ControleDeBar.Dominio/ModuloConta/IRepositorioConta.cs
@@ -0,0 +1,13 @@
+﻿namespace ControleDeBar.Dominio.ModuloConta;
+public interface IRepositorioConta 
+{
+    void Inserir(Conta conta);
+    bool AtualizarPedidos(Conta conta, List<Pedido> pedidosRemovidos);
+    void AtualizarStatus(Conta contaFechada);
+
+    Conta SelecionarPorId(int id);
+    List<Conta> SelecionarContas();
+    List<Conta> SelecionarContasEmAberto();
+    List<Conta> SelecionarContasFechadas();
+    List<Conta> SelecionarContasFaturamento();
+}
